f3e130d9da30082145fd909d2d3a8a12d73fc4c5
alwe009 <63882279+alwe009@users.noreply.github.com>
2022-09-13 14:11:26 +0800

    cart
    cart

--- src/firebase.js
+++ src/firebase.js
@@ -1,25 +1,24 @@
-import firebase from 'firebase/compat/app'
-import 'firebase/compat/auth';
-import 'firebase/compat/firestore';
+import firebase from "firebase/compat/app";
+import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyC3hEA8ehSAg1pdwYmWmKLPBi2HOMcfQ9k",
-    authDomain: "react-emart-9c07c.firebaseapp.com",
-    projectId: "react-emart-9c07c",
-    storageBucket: "react-emart-9c07c.appspot.com",
-    messagingSenderId: "745337420024",
-    appId: "1:745337420024:web:0cf2f9425d7161819fe30d"
-  };
+  apiKey: "AIzaSyB5faE8aN7oBsSkMCNvw_xDZZpwW0XXxeo",
+  authDomain: "react-emart-a5ddd.firebaseapp.com",
+  projectId: "react-emart-a5ddd",
+  storageBucket: "react-emart-a5ddd.appspot.com",
+  messagingSenderId: "1042075282848",
+  appId: "1:1042075282848:web:52bdc02a47e6a1dce1c8c5",
+};
 
 // Use this to initialize the firebase App
 const firebaseapp = firebase.initializeApp(firebaseConfig);
 
 // Use for db
-const db = firebaseapp.firestore()
+const db = firebaseapp.firestore();
 const auth = firebase.auth();
 const googleProvider = new firebase.auth.GoogleAuthProvider();
 const facebookProvider = new firebase.auth.FacebookAuthProvider();
 
-export { db, auth, googleProvider, facebookProvider } 
-  
+export { db, auth, googleProvider, facebookProvider };
